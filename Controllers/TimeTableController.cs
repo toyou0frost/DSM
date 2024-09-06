d46dc9cfec92f716b5340808a572a9751b401e30
butdory <butdory04@gmail.com>
2024-09-06 11:43:13 +0900

    2024-09-06-03

--- Controllers/TimeTableController.cs
+++ Controllers/TimeTableController.cs
@@ -16,10 +16,12 @@ public class TimeTableController : Controller
     public IActionResult TimeTable()
     {    
         var TimeTableDay = new List<TIMETABLE_CLS>{
-            new TIMETABLE_CLS { SubjectName = "말하기와 사고", ProfessorName = "Mr. Kim" },
-            new TIMETABLE_CLS { SubjectName = "임상병리 기초", ProfessorName = "Ms. Lee" },
-            new TIMETABLE_CLS { SubjectName = "임상병리 기초", ProfessorName = "Ms. Lee" },
-            new TIMETABLE_CLS { SubjectName = "임상병리 기초", ProfessorName = "Ms. Lee" },
+            new TIMETABLE_CLS { Time = "2교시", Period = "10:10 ~ 11:00", Memo = "의료법 기초"},
+            new TIMETABLE_CLS { Time = "3교시", Period = "11:00 ~ 12:00", Memo = "의료법 기초"},
+            new TIMETABLE_CLS { Time = "4교시", Period = "12:00 ~ 12:50", Memo = "의료법 기초"},
+            new TIMETABLE_CLS { Time = "점심", Period = "13:00 ~ ", Memo = "식단표"},
+            new TIMETABLE_CLS { Time = "6교시", Period = "15:10 ~ 16:00", Memo = "말하기와 사고"},
+            new TIMETABLE_CLS { Time = "7교시", Period = "16:00 ~ 16:50", Memo = "말하기와 사고"},
         };
 
         return View(TimeTableDay);
